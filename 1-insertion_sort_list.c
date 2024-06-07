11515f2f35fb6bb02ad2a2aac0b45d4790f2d801
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-07 17:02:48 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,18 +7,7 @@
  * @previous: pointer to previous node of list.
  */
 
-void swap(listint_t **list, listint_t *current, listint_t *previous)
-{
-	if (current->next)
-		(current->next)->prev = previous;
-	current->prev = previous->prev;
-	current->next = previous;
 
-	if (previous->prev)
-		(previous->prev)->next = current;
-	else
-		(*list) = current;
-}
 /**
  * insertion_sort_list - Sort a list with insertion sort algorithm.
  * @list: list of integers

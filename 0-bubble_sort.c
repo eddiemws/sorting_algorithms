982f58be8de57ddf5d40b258291f5fe2bb240855
eddy <106661971+eddiemws@users.noreply.github.com>
2024-06-26 23:58:37 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,8 @@
 #include "sort.h"
 
-/**
- * swap - Swap values of start and next index.
- * @array: the array with start and next values.
- * @start: start point of the array.
- * @next: next point of the array.
- * Return: nothing.
- */
+
+array[start] = temp;
+}
 void swap(int *array, size_t start, size_t next)
 {
 	size_t temp = array[next];

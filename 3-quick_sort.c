0acb00a0fe78dafa2cc0de27bfd0c6ebe448e2bb
eddy <106661971+eddiemws@users.noreply.github.com>
2024-10-16 22:45:04 +0300

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -45,5 +45,12 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
     }
 }
 
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_recursive(array, 0, size - 1, size);
+}
 
 
